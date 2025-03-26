9117caaade1cc234b214cc5117e8b9ba46eecda4
Miles <yaomengsi@outlook.com>
2025-03-26 22:30:38 +0800

    add go, ai

--- lua/community.lua
+++ lua/community.lua
@@ -12,12 +12,15 @@ return {
   { import = "astrocommunity.git.git-blame-nvim" },
   { import = "astrocommunity.indent.indent-rainbowline" },
   { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.motion.marks-nvim" },
   -- ai completion
   { import = "astrocommunity.completion.avante-nvim" },
   -- language
   { import = "astrocommunity.pack.python-ruff" },
   { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.go" },
   -- ui app
+  { import = "astrocommunity.recipes.ai" },
   { import = "astrocommunity.recipes.neovide" },
   { import = "astrocommunity.recipes.vscode" },
 }
